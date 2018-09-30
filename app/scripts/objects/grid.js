89ee180930e0309c0e1cf11807115320336e0878
DESKTOP-3SE1LP0\Mike <whitaker.mj@gmail.com>
2018-09-30 09:24:43 +0200

    intial repo fill

---
+++ app/scripts/objects/grid.js
@@ -0,0 +1,25 @@
+export default class Grid extends Phaser.GameObjects.Sprite {
+  /**
+   *  A simple prefab (extended game object class), displaying a spinning
+   *  Phaser 3 logo.
+   *
+   *  @extends Phaser.GameObjects.Sprite
+   */
+  constructor(scene) {
+    super(scene, 0, 0, 'grid');
+
+    const x = scene.cameras.main.width / 2;
+    const y = scene.cameras.main.height / 2;
+
+    this.setPosition(x, y);
+    this.setOrigin(0.5);
+  }
+
+  /**
+   *  Increment the angle smoothly.
+   */
+  update() {
+    // Get called from main.js update
+    // this.angle += 0.1;
+  }
+}
