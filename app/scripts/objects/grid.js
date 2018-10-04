f7c7b7551bad01f07f946971209442515595c16c
DESKTOP-3SE1LP0\Mike <whitaker.mj@gmail.com>
2018-10-04 06:19:28 +0200

    no message

--- app/scripts/objects/grid.js
+++ app/scripts/objects/grid.js
@@ -13,7 +13,92 @@ export default class Grid extends Phaser.GameObjects.Sprite {
 
     this.setPosition(x, y);
     this.setOrigin(0.5);
+    this.grid = {};
+    
   }
+
+  create() {
+    var grid = function(xSize, ySize) {
+      //initialize the grid.
+      var gridArray = [];
+      var dimensions = null;
+      var listOfCells = [];
+  
+      var Dimensions = function(row, col) {
+          var x = col;
+          var y = row;
+          return {
+            colSize: x,
+            rowSize: y
+          };
+        },
+        getGridArray = function() {
+          return gridArray;
+        },
+        setGridArray = function(anArray) {
+          gridArray = anArray;
+        },
+        getDimensions = function() {
+          return dimensions;
+        },
+        getListOfCells = function() {
+          return listOfCells;
+        };
+  
+      dimensions = new Dimensions(ySize, xSize);
+  
+      return {
+        getDimensions: getDimensions,
+        getGridArray: getGridArray,
+        getListOfCells: getListOfCells,
+        setGridArray: setGridArray
+      };
+    }();
+  
+    var point = function(col, row) {
+      var x = col,
+        y = row,
+        getCoOrdinates = function() {
+          return {
+            x: x,
+            y: y
+          };
+        };
+  
+      return {
+        getCoOrdinates: getCoOrdinates
+      };
+    };
+  
+    var Row = function(xSize, yCurrentRow) {
+      var row = [];
+      var gameGrid = vm.gameGrid.getListOfCells();
+      for (let i = 0; i < xSize; i++) {
+        var aCell = new Cell(yCurrentRow, i);
+        // one in five should generate a living cell.
+        // others should be dead but still present.
+        let rnd = Math.random() * 10;
+        if (rnd < 0.8) {
+          row.push(aCell);
+          aCell.live();
+        } else {
+          aCell.die();
+          row.push(aCell);
+        }
+        // pop a reference to the cell in a list as well for convenience
+        gameGrid.push(aCell);
+      }
+  
+      var getRow = function() {
+        return row;
+      };
+  
+      return {
+        getRow: getRow
+      };
+    };
+  }
+  
 
   /**
    *  Increment the angle smoothly.
